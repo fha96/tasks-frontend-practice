0a8d698a294944ff69a0068ebd01333024a0de65
fha96 <fahadalrimawe96@hotmail.com>
2022-10-03 19:10:23 +0300

    edit

--- __test__/Context.test.js
+++ __test__/Context.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render, fireEvent, waitFor, screen, getByTestId, getByText } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
-import LoginContextProvider, { LoginContext } from "../context/LoginContext";
-import { Signin } from "../components/registration/Signin";
+import LoginContextProvider, { LoginContext } from "../src/context/LoginContext";
+import { Signin } from "../src/components/registration/Signin";
 test("Load page", async () => {
   render(
     <LoginContextProvider>
