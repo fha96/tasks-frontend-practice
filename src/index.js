55ba4d1c19800732f42bfe9be89ae17548f49b4f
fha96 <fahadalrimawe96@hotmail.com>
2022-11-21 17:49:46 +0200

    refactor(Application) : add redux to manage login action

--- src/index.js
+++ src/index.js
@@ -7,12 +7,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import LoginReducer from './features/registration';
+
+const store = configureStore({
+  reducer:{
+    LoginReducer
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
+        <Provider store={store}>
         <App />
+        </Provider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
